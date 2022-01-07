77df5e08da7179f64681dbd98234decee408e626
moonjiyoon <ske0830@gmail.com>
2022-01-07 20:46:33 +0900

    modified Beakjoon 10816

--- Baekjoon/Hashing/10816.py
+++ Baekjoon/Hashing/10816.py
@@ -3,18 +3,19 @@
 
 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드가 몇 개 인지 구하라.
 '''
+import collections
 
 N = int(input())
 number_card = list(map(int, input().split()))
 M = int(input())
 numbers = list(map(int, input().split()))
 
-numbers_dict = {}
-for number in numbers:
-    numbers_dict[number] = 0
-
+numbers_dict = collections.defaultdict(int)
 for number in number_card:
+    numbers_dict[number] += 1
+
+for number in numbers:
     if number in numbers_dict.keys():
-        numbers_dict[number] += 1
-    
-print(*numbers_dict.values(), sep=" ")
+       print(numbers_dict[number], end=" ")
+    else:
+        print(0, end=" ")
