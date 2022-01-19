a0c2ba708fe6d8ed414a5e798390acbf526627e1
moonjiyoon <ske0830@gmail.com>
2022-01-19 19:51:04 +0900

    modified beakjoon 11053, 18353

--- Baekjoon/DynamicProgramming/11053.py
+++ Baekjoon/DynamicProgramming/11053.py
@@ -1,16 +1,16 @@
 '''가장 긴 증가하는 부분 수열'''
 
-N = int(input())
-array = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-d = [array[0]]
+N = int(input())
+l = list(map(int, input().split()))
+
+dp = [1] * N
+
 for i in range(1, N):
-    if array[i] > d[-1]:
-        d.append(array[i])
-    if len(d) > 1 and d[-2] < array[i] < d[-1]:
-        d.pop()
-        d.append(array[i])
-    if len(d) == 1 and array[i] < d[-1]:
-        d[0] = array[i]
-        
-print(len(d))
+    for j in range(i):
+        if l[j] < l[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
--- Baekjoon/DynamicProgramming/18353.py
+++ Baekjoon/DynamicProgramming/18353.py
@@ -1,2 +1,15 @@
 '''병사 배치하기'''
 
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+soldiers = list(map(int, input().split()))
+dp = [1] * N
+
+for i in range(N):
+    for j in range(i):
+        if soldiers[j] > soldiers[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(N - max(dp))
