10568f30e22df74796418a7f2d3272c5c3d3b46a
moonjiyoon <ske0830@gmail.com>
2022-02-18 15:42:56 +0900

    Java Baek 14502

---
+++ Java/DFS_BFS/Baek14502.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek14502 {
+    public static void main(String arg[]) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+
+    }
+}
