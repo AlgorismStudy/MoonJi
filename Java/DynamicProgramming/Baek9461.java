8f42f5b6c532ae951bd1a14811404ee07296f22b
moonjiyoon <ske0830@gmail.com>
2022-03-04 20:52:23 +0900

    java Baek 9461

--- Java/DynamicProgramming/Baek9461.java
+++ Java/DynamicProgramming/Baek9461.java
@@ -1,2 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Baek9461 {
+    static long[] pado;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int T = Integer.parseInt(s);
+
+        for(int i=0; i<T; i++) {
+            s = br.readLine();
+            int N = Integer.parseInt(s);
+            pado = new long[N+1];
+            System.out.println(funcPado(N));
+        }
+
+
+        br.close();
+    }
+    static long funcPado(int N) {
+        for(int i=1; i <= N; i++) {
+            if(i == 1 || i == 2 || i == 3) pado[i] = 1L;
+            else pado[i] = pado[i-2] + pado[i-3];
+        }
+        return pado[N];
+    }
 }
