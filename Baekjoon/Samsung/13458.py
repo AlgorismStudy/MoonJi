7bd36d57c27f1d6938ac91ef92a9a7f40febfa31
moonjiyoon <ske0830@gmail.com>
2022-03-22 16:59:33 +0900

    Samsung Baek 13458

---
+++ Baekjoon/Samsung/13458.py
@@ -0,0 +1,17 @@
+'''시험 감독'''
+
+N = int(input())
+A = list(map(int, input().split()))
+B, C = map(int, input().split())
+
+result = 0
+A = [n - B for n in A]
+result += N
+
+for a in A:
+    if a > 0:
+        q = a // C
+        r = a % C
+        result += q if r == 0 else q + 1
+
+print(result)
