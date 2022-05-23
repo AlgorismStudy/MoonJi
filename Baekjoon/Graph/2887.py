b70b8d4f0146a5c6de0f88add9aa8ecd24a84dad
moonjiyoon <ske0830@gmail.com>
2022-05-23 13:24:51 +0900

    Baek 2887

--- Baekjoon/Graph/2887.py
+++ Baekjoon/Graph/2887.py
@@ -1,51 +1,46 @@
 '''행성 터널'''
 
-def find_parent(parent, x):
+import sys
+input = sys.stdin.readline
+
+def find(x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
+        return find(parent[x])
     return parent[x]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+def union(a, b):
+    a = find(a)
+    b = find(b)
 
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
-def make_cost(star_a, star_b):
-    cost = 987654321
-    for a, b in zip(star_a, star_b):
-        if abs(a - b) < cost:
-            cost = abs(a - b)
-    return cost 
-
 N = int(input())
-parent = [0] * (N + 1)
-
-stars = []
-for _ in range(N):
-    x, y, z = map(int, input().split())
-    stars.append((x, y, z))
-
+parent = [i for i in range(N)]
 edges = []
-result = 0
+stars = []
 
-for i in range(1, N + 1):
-    parent[i] = i
+for i in range(N):
+    x, y, z = map(int, input().split())
+    stars.append((x, y, z, i))
 
-for i in range(N-1):
-    for j in range(i, N):
-        cost = make_cost(stars[i], stars[j])
-        edges.append((cost, i, j))
+for j in range(3):
+    stars.sort(key=lambda x:x[j])
+    before = stars[0][3]
+    for i in range(1, N):
+        cur = stars[i][3]
+        edges.append([abs(stars[i][j] - stars[i-1][j]), before, cur])
+        before = cur
 
 edges.sort()
 
+result = 0
 for edge in edges:
     cost, a, b = edge
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
+    if find(a) != find(b):
+        union(a, b)
         result += cost
 
 print(result)
