e7868537dc828045ffbdef92834742240d9e9499
moonjiyoon <ske0830@gmail.com>
2022-05-26 00:46:09 +0900

    Programmers 신고 결과 받기

---
+++ Programmers/2022_KakaoBlindRecruitment/신고결과받기.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+def solution(id_list, report, k):
+    answer = []
+    mail = defaultdict(int)
+    d = defaultdict(list)
+    
+    for r in report:
+        a, b = r.split()
+        if a not in d[b]:
+            d[b].append(a)
+        
+    for key, value in d.items():
+        if len(value) >= k:
+            for v in value:
+                mail[v] += 1           
+    for id in id_list:
+        if mail[id] > 0:
+            answer.append(mail[id])
+        else:
+            answer.append(0)
+    
+    return answer
